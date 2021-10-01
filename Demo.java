aa42b501a3abdafc345aed5b092422cbae734be4
Rohan-s18 <68778986+Rohan-s18@users.noreply.github.com>
2021-10-01 14:48:46 -0400

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package com.rohan.homework.assignment2;
+
+import java.util.*;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("\n");
+		
+		
+		
+	}
+
+}
---
+++ ReverseLinkedList_Demo.java
@@ -0,0 +1,25 @@
+package com.rohan.homework.assignment2;
+
+public class ReverseLinkedList_Demo {
+
+	public static void main(String[] args) {
+		NumLinkedList myTestList = new NumLinkedList();
+		myTestList.add(1);
+		myTestList.add(2);
+		myTestList.add(3);
+		myTestList.add(4);
+		myTestList.add(5);
+		myTestList.add(6);
+		myTestList.add(7);
+		myTestList.add(8);
+		myTestList.add(9);
+		myTestList.printList();			//Printing the original order of the list
+		
+		System.out.println();
+		
+		myTestList.reverse();			//Calling the reverse method
+		myTestList.printList();			//Printing the reversed order of the list
+
+	}
+
+}
