40c4310a132a72395e85b3db0893b264d2dd9aab
Rohan-s18 <68778986+Rohan-s18@users.noreply.github.com>
2021-11-23 20:48:10 -0500

    Update Sort.java

--- Homework/src/com/rohan/homework/assignment5/Sort.java
+++ Homework/src/com/rohan/homework/assignment5/Sort.java
@@ -1,40 +1,75 @@
 package com.rohan.homework.assignment5;
 
-import java.util.Vector;
 
 public class Sort {
-	private int numElements;
 	private int depth;
 	private boolean sorted;
 	
 	public static void main(String[]args) {
 		Sort mySortingTest = new Sort();
-		int[] arry = new int[20];
-		for(int i = 0; i < 20; i++) {
-			arry[i] = (int)(Math.random()*100);
-		}
+		int[] arry = new int[10];
+		
+		System.out.println("Before Insertion Sort:");
+		mySortingTest.generateRandomArray(arry);
 		mySortingTest.printArray(arry);
+		System.out.println("After Insertion Sort:");
+		mySortingTest.insertionSort(arry);
+		mySortingTest.printArray(arry);
+		System.out.println("\n");
+		
+		System.out.println("Before Merge Sort:");
+		mySortingTest.generateRandomArray(arry);
+		mySortingTest.printArray(arry);
+		System.out.println("After Merge Sort:");
+		mySortingTest.mergeSort(arry);
+		mySortingTest.printArray(arry);
+		System.out.println("\n");
+		
+		System.out.println("Before Quick Sort:");
+		mySortingTest.generateRandomArray(arry);
+		mySortingTest.printArray(arry);
+		System.out.println("After Quick Sort:");
+		mySortingTest.quickSort(arry);
+		mySortingTest.printArray(arry);
+		System.out.println("\n");
+		
+		System.out.println("Before Upgraded Quick Sort:");
+		mySortingTest.generateRandomArray(arry);
+		mySortingTest.printArray(arry);
+		System.out.println("After Upgraded Quick Sort:");
 		mySortingTest.upgradedQuickSort(arry,1,1);
-		//System.out.println(mySortingTest.select(arry, 5));
 		mySortingTest.printArray(arry);
+		System.out.println("\n");
+		
+		int k = 0;
+		System.out.println("Array:");
+		mySortingTest.generateRandomArray(arry);
+		mySortingTest.printArray(arry);
+		int x = mySortingTest.select(arry,k);
+		System.out.println(String.valueOf(k)+"th largest element is: "+String.valueOf(x));
+		System.out.println("\n");
+		
+		
 	}
 	
 	
-	
+	//InsertionSort method starts here
 	public void insertionSort(int[] arr) {
 		for (int i = 1; i < arr.length; i++) {
-			int toInsert = arr[i];
+			int temp = arr[i];
 			int j = 0;
-			for (j = i; j > 0 && toInsert > arr[j-1]; j--) 
+			for (j = i; j > 0 && temp > arr[j-1]; j--) 
 				arr[j] = arr[j-1];
-			arr[j] = toInsert;
+			arr[j] = temp;
 		}
 	}
-
+	
+	
+	//Start of Merge Sort and helpers
 	public void mergeSort(int[] arr) {
 		int[] temp = new int[arr.length];
 		myMergeSort(arr, temp, 0, arr.length - 1);
-		}
+	}
 
 	private void myMergeSort(int[] arr, int[] temp, int start, int end) {
 		if (start >= end ) // base case
@@ -70,12 +105,14 @@ public class Sort {
 			j++;
 			k++;
 		}
-		
 		for(int index = leftStart; index <= rightEnd; index++)
 			arr[index] = temp[index];
 		
 	}
+	//End of Merge sort and helpers
+	
 
+	//Start of Quick sort and helpers
 	public void quickSort(int[] arr) {
 		myQuickSort(arr,0,arr.length-1);
 	}
@@ -107,10 +144,10 @@ public class Sort {
 				return j; 
 		}
 	}
+	//End of Quick sort and helpers
 	
-	//Upgraded Quicksort 
 	
-	
+	//Start of Upgraded Quick sort and helpers 
 	public void upgradedQuickSort(int[] arr, int k, int d) {
 		sorted  = false;
 		myUpgradedQuickSortHelper(arr,0,arr.length-1,k,d);
@@ -138,66 +175,23 @@ public class Sort {
 		myUpgradedQuickSortHelper(arr,first,split,k,d);
 		myUpgradedQuickSortHelper(arr,split+1,last,k,d);
 	}
+	//End of Upgraded Quick sort and helpers
 	
 	
-	//Select Method
+	//Start of select method and helpers
 	public int select(int[] arr, int k) {
-		if(k >= arr.length)
+		int len = arr.length;
+		if(k >= len)
 			System.out.println("K must be within the bounds of the array");
 		bucketSort(arr);
-		return arr[k];
+		return arr[len-k-1];
 	}
 	
-	private void vectorInsertionSort(Vector<Integer> vec) {
-		for (int i = 1; i < vec.size(); i++) {
-			int toInsert = vec.get(i);
-			int j = 0;
-			for (j = i; j > 0 && toInsert > vec.get(j-1); j--) 
-				vec.set(j, vec.get(j-1));
-			vec.set(j, toInsert);
-		}	
-		
-	}
-	
-	private void bucketSort(int[] arr, int n) {
-	        if (n <= 0)
-	            return;
-	 
-	        // 1) Create n empty buckets
-	        Vector<Integer>[] buckets = new Vector[n];
-	 
-	        for (int i = 0; i < n; i++) {
-	            buckets[i] = new Vector<Integer>();
-	        }
-	 
-	        // 2) Put array elements in different buckets
-	        for (int i = 0; i < n; i++) {
-	            int idx = arr[i]%n;
-	            buckets[idx].add(arr[i]);
-	        }
-	 
-	        // 3) Sort individual buckets
-	        for (int i = 0; i < n; i++) {
-	            vectorInsertionSort(buckets[i]);
-	        }
-	 
-	        // 4) Concatenate all buckets into arr[]
-	        int index = 0;
-	        for (int i = 0; i < n; i++) {
-	            for (int j = 0; j < buckets[i].size(); j++) {
-	                arr[index++] = buckets[i].get(j);
-	            }
-	        }
-	}
 	
 	private void bucketSort(int[]arr) {
-		Integer[] arr1 = new Integer[arr.length];
-		for(int i = 0; i < arr.length; i++)
-			arr1[i] = arr[i];
-		Integer[] temp = new Integer[(int)Math.pow(2,16)];
-		for(int i = 0; i < arr1.length; i++) {
-			temp[arr[i]] = arr1[i];
-		}
+		Integer[] temp = new Integer[200];
+		for(int i = 0; i < arr.length; i++) 
+			temp[arr[i]] = arr[i];
 		int j = 0;
 		int k = 0;
 		while(j<arr.length && k<temp.length) {
@@ -208,9 +202,9 @@ public class Sort {
 			k++;	
 		}
 	}
+	//End of select method and helpers
 	
 	//Other extra methods
-	
 	public void printArray(int[] arr) {
 		String s = "[";
 		for(int i = 0; i < arr.length; i++) {
@@ -228,5 +222,12 @@ public class Sort {
 		arr[i] = arr[j];
 		arr[j] = temp;
 	}
+	
+	private void generateRandomArray(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = (int)(Math.random()*200);
+		}
+	}
 
 }
+
