84c8d778917c4d171f4a8f60363460ada2ec7aac
Rohan Singh <rohansingh@Rohans-MacBook-Pro.local>
2022-10-14 14:40:04 -0400

    Created a folder for Stacks, Queues and Trees

---
+++ Stacks and Queues/StackArray.java
@@ -0,0 +1,48 @@
+package com.rohan.csds233.selfpractice.basics;
+
+import java.util.*;
+
+public class StackArray{
+	private Object[] myStack;
+	private int top;
+	
+	public StackArray(int maxSize) {
+		myStack = new Object[maxSize];
+		top = -1;
+	}
+	
+	public StackArray() {
+		myStack = new Object[10];
+		top = -1;
+	}
+	
+	public boolean push(Object e) {
+		if(top < myStack.length - 1) {
+			top++;
+			myStack[top] = e;
+			return true;
+		}
+		
+		return false;		
+	}
+	
+	public Object pop() {
+		if(isEmpty()) {
+			return null;
+		}
+		return myStack[top];
+	}
+	
+	public boolean isEmpty() {
+		if(top==-1)
+			return true;
+		return false;
+	}
+	
+}
+
+
+
+
+
+
---
+++ Trees/BinarySearchTree_Demo.java
@@ -0,0 +1,29 @@
+package com.rohan.homework.assignment3;
+
+public class BinarySearchTree_Demo {
+	
+	public static void main(String[] args) {
+		BinarySearchTree<String> testTree = new BinarySearchTree<String>();
+		//Inserting some elements
+		testTree.insert(44);
+		testTree.insert(71);
+		testTree.insert(3);
+		testTree.insert(19);
+		testTree.insert(26);
+		testTree.insert(61);
+		testTree.insert(87);
+		testTree.insert(5);
+		testTree.inorder();			//Performing inorder traversal (should be ascending order)
+		System.out.println();
+		testTree.sum();				//Performing the Sum operation 
+		System.out.print("\n");
+		System.out.println(testTree.kthSmallest(5));		//Performing KthSmallest operation
+		System.out.print("\n");
+		System.out.println(testTree.delete(61));		//Performing delete operation
+		System.out.print("\n");
+		System.out.println(testTree.search(0));			//Searching for an element that doesn't exist
+		System.out.println(testTree.search(19));
+		System.out.print("\n");
+		testTree.inorder();
+	}
+}
