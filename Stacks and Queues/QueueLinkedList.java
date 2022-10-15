28939add1619dbf815d2a2c23f53c594c03c6639
Rohan Singh <68778986+Rohan-s18@users.noreply.github.com>
2022-10-15 00:07:17 -0400

    Made changes in the Generic types

--- Stacks and Queues/QueueLinkedList.java
+++ Stacks and Queues/QueueLinkedList.java
@@ -1,62 +1,107 @@
 
-public class QueueLinkedList{
-	private Node head;
-	private Node tail;
-	
-	public QueueLinkedList() {
-		head = new Node();
-		tail = new Node();
-		head.setNext(tail);
+public class QueueLinkedList<T>{
+
+	//These are the pointers to the head and tail of the Queue, check the Node<T> class at the bottom
+	private Node<T> head;
+	private Node<T> tail;
+
+
+	//Main method (just for demo)
+	public static void main(String[] args){
+		QueueLinkedList<Integer> q = new QueueLinkedList<Integer>();
+
+		q.enque(1);
+		q.enque(2);
+		q.enque(3);
+		q.enque(4);
+		q.enque(5);
+
+		System.out.println(q.deque());
+		System.out.println(q.deque());
+		System.out.println(q.deque());
+		System.out.println(q.deque());
+		System.out.println(q.deque());
+		
+
+
 	}
 	
-	public void enque(Object e) {
-		Node temp = new Node();
-		temp.setContent(e);
-		if(tail == null)
+	public void enque(T e) {
+		//Creating a node for the new value
+		Node<T> temp = new Node<T>();
+		temp.setVal(e);
+
+		//If nothing has been added to the LinkedList yet, then the head is equal to the tail
+		if(head == null){
 			head = temp;
+			tail = head;
+			return;
+		}
+
+		//Setting the next value of the tail to temp and also changing the pointer to the tail, to the one after it
 		tail.setNext(temp);
 		tail = tail.getNext();
+
+		
 	}
 	
-	public Node deque() {
-		Node temp = head.getNext();
-		head.setNext(head.getNext().getNext());
+	public T deque() {
+		//Edge case
+		if(head == null)
+			return null;
+		
+		//Returning the head value
+		T temp = head.getVal();
+		
+		//Moving the head to one place forward
+		head = head.getNext();
 		return temp;
 	}
 	
 	public void print() {
-		Node trav = head.getNext();
+		Node<T> trav = head;
 		while(trav != null) {
-			System.out.println(trav.getContent());
+			System.out.println(trav.getVal());
 			trav = trav.getNext();
 		}
 	}
 	
-	public Node getHead() {
+	/*
+	private Node getHead() {
 		if(head == null)
 			return null;
 		return head;
 	}
+	*/
+
+	class Node<T>{
+		//Pointer to the next node
+		private Node<T> next;
+
+		//This will store the content of the node
+		private T content;
+
+		public void setNext(Node<T> next) {
+			this.next = next;
+		}
+
+		public void setVal(T val) {
+			this.content = val;
+		}
+
+		public T getVal() {
+			return content;
+		}
+
+		public Node<T> getNext() {
+			return next;
+		}
+
+		public String toString() {
+			return content.toString();
+		}
+	}
 
 }
 
 
-class Node{
-	private Node next;
-	private Object content;
-	public void setNext(Node next) {
-		this.next = next;
-	}
-	public void setContent(Object content) {
-		this.content = content;
-	}
-	public Object getContent() {
-		return content;
-	}
-	public Node getNext() {
-		return next;
-	}
-	public String toString() {
-		return content.toString();
-	}
-}
