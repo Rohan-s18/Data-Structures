a87200bb5f0d4cd03b0735d89e8d7eba904bc4ae
Rohan Singh <68778986+Rohan-s18@users.noreply.github.com>
2022-12-01 02:27:14 -0500

    Add function for vertices and edges for the unweighted graph

--- Graphs/UndirectedGraph.java
+++ Graphs/UndirectedGraph.java
@@ -15,11 +15,57 @@ public class UndirectedGraph{
     //Optional array to hold the index of parents (used during traversals)
     //private int[] parents;
 
+    //Constructor
     public UndirectedGraph(){
         graph = new boolean[10][10];
         names = new String[10];
         size = 0;
     }
+
+    //Overloaded Constructor
+    public UndirectedGraph(capacity){
+        graph = new boolean[capacity][capacity];
+        names = new String[capacity];
+        size = 0;
+    }
+
+    //Method to add a vertex/node to the graph
+    public boolean addVertex(String name){
+        //Checking if we have overloaded
+        if(size >= names.length)
+            resize();
+        
+        //Checking to see if the node exists by using the index of helper function
+        if(indexOf(name) != -1)
+            return false;           //Could throw an exception here
+
+        //Adding a node of the same name to the graph
+        names[size] = name;
+        size++;
+
+        return true;
+    }
+
+    //Method to add an edge/link to the graph between vertices a and b
+    public boolean addEdge(String a, String b){
+        //Getting the indices of the nodes
+        int firstNodeIndex = indexOf(a);
+        int secondNodeIndex = indexOf(b);
+
+        //Checking to see if the vertices exist or not
+        if(firstNodeIndex == -1 || secondNodeIndex == -1)
+            return false;
+
+        //Setting the connectivity to true
+        graph[firstNodeIndex][secondNodeIndex] = true;
+        graph[secondNodeIndex][firstNodeIndex] = true;
+
+        return true;
+
+    }
+
+
+    //General Helper functions for the graph to make our life easier
 
     //This method will resize the matrix if it gets full
     private void resize(){
