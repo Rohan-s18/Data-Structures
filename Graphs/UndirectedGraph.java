559de75c70ee398e5c6fef80c5726c91bc1e0b49
Rohan Singh <68778986+Rohan-s18@users.noreply.github.com>
2022-12-01 23:35:45 -0500

    Added code for BFS for undirected graphs

--- Graphs/UndirectedGraph.java
+++ Graphs/UndirectedGraph.java
@@ -82,6 +82,52 @@ public class UndirectedGraph{
         return true;
     }
 
+    //Method for Breadth-First Traversal of a graph from the source
+    public void breadthFirstTraversal(String source){
+        int sourceIndex = indexOf(source);
+        //Checking if the source exists
+        if(sourceIndex == -1)
+            return;
+
+        //Creating an array to track the parents of the vertices in BFT
+        int[] parents = new int[size];
+
+        //Creating an array to see which vertices have been visited/encountered during the traversal
+        boolean[] encountered = new boolean[size];
+
+        //Queue that will be used for the breadth first traversal of the graph
+        Queue<Integer> traversalQueue = new LinkedList<Integer>();
+
+        //Initializing the data structures for the traversal
+        traversalQueue.add(sourceIndex);
+        encountered[sourceIndex] = true;
+        parents[sourceIndex] = -1;
+
+        //Iterating while the queue is not empty
+        while(!traversalQueue.isEmpty()){
+            //Removing the item from the queue
+            int curr = traversalQueue.poll();
+
+            //Other actions can be done here
+            System.out.println(names[curr]);
+
+            //Iterating through all of the neighbors of the current vertex
+            for(int j = 0; j < size; j++){
+                //If an edge exists and the destination has not been encountered
+                if(encountered[j] == false && graph[curr][j] == true){
+                    encountered[j] = true;                                  //Now this vertex has been encountered
+                    parents[j] = curr;                                      //Setting its parent
+                    traversalQueue.add(j);                                  //Adding it to the queue
+                }
+            }
+
+        }
+
+
+
+    }
+
+
 
     //General Helper functions for the graph to make our life easier
 
